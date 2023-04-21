351b2420d99670753fc70c65a2a6aed6d260341b
satyam kale <satyamkale271@gmail.com>
2023-04-21 19:59:04 +0530

    save feature add

--- passenger-app/passengers.js
+++ passenger-app/passengers.js
@@ -1,12 +1,14 @@
 
  let countEl = document.getElementById("count-el")
-
-let count = 0
-
-
+ let strel = document.getElementById("entries")
+ let count = 0
+ 
  function increment() {
     count = count + 1
      countEl.innerText = count
  }
 
-
+ function save() {
+    let countstrr = count + " - "
+    strel.innerText = strel.innerText + countstrr
+ }
