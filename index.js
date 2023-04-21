ad53b7b27c187f8506083480ce2be052b05cb1cc
satyam kale <satyamkale271@gmail.com>
2023-04-21 18:54:29 +0530

    index.js updated

--- index.js
+++ index.js
@@ -9,7 +9,14 @@
 // console.log(namee +", " + surname)
 
 // **** strings and number ***** //
-let age = 19
-let message = "my age is "
-let together = message + age
-console.log(together)
+// let age = 19
+// let message = "my age is "
+// let together = message + age
+// console.log(together)
+//  if there is one number and one string the number 
+//  will be converted to string 
+ // example:-
+ let num = 5
+ let nums =  "5"
+ console.log(num + nums) // output is showing 55 not 10 beacuase number us converted to string//
+ 
