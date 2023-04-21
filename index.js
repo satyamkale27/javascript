964ab77fbff5f1ee8fd05e36b844bad54cffab1b
satyam kale <satyamkale271@gmail.com>
2023-04-21 18:47:11 +0530

    strings and number

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+// let age = 19
+// let dogage = 7
+// let humanage = age * dogage
+// console.log(humanage);
+// stri=ings in javascript //
+
+// let namee = "steve"
+// let surname = "smith"
+// console.log(namee +", " + surname)
+
+// **** strings and number ***** //
 let age = 19
-let dogage = 7
-let humanage = age * dogage
-console.log(humanage);
+let message = "my age is "
+let together = message + age
+console.log(together)
