7b841586e0f4a8781352a27296a9c983c251fe1d
satyam kale <satyamkale271@gmail.com>
2023-04-23 15:12:16 +0530

    concept.js updated

---
+++ javascript-concepts/concept.js
@@ -0,0 +1,19 @@
+let mypoint = 3
+
+function addpoint() {
+    mypoint = mypoint + 3
+    // this cam be also written like mypoint += 3 //
+    
+}
+
+function removalpoint() {
+    mypoint = mypoint - 2
+    // this cam be also written like mypoint -= 2 //
+}
+
+ addpoint()  // here we called the function //
+ addpoint()  // here we called the function //
+ addpoint()  // here we called the function //
+ removalpoint()  // here we called the function //
+
+console.log(mypoint)
