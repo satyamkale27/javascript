bb938a52c15f5a57c5879214c33f42d5c3db5e4f
satyam kale <satyamkale271@gmail.com>
2023-04-23 15:26:01 +0530

    addition of string and number(indexx.js)

---
+++ javascript-concepts/indexx.js
@@ -0,0 +1,6 @@
+// addition of strings and numbers and text //
+console.log("2" + 2)                    // output is 22 //
+console.log(11 + 7)                     // output is 18 //
+console.log(6 + "5")                    // output is 65 //
+console.log("my points: " + 5 + 9)      // output is      my points: 59   //
+console.log("11" + "11")                // output is 1111
