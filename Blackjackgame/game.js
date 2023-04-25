725877aa40f58a691c65ee01879c9e0e410fb3db
satyam kale <satyamkale271@gmail.com>
2023-04-25 19:47:23 +0530

    upadated

--- Blackjackgame/game.js
+++ Blackjackgame/game.js
@@ -1,19 +1,26 @@
+let instructionEl = document.getElementById("instruction")
+let sumEl = document.getElementById("summ")
+let cardEl = document.getElementById("cardcount")
+
 let fristcard = 10
 let secondcard = 11
 let sum = fristcard + secondcard
 let hasblackjack = false    // this is boolean type value learned in this project //
 let isalive = true
 let message = ""
+
 function startgame() {
-if(sum < 22) {
-    message = "Do you want to draw a new card"
-} else if(sum === 2){
-message = "wohoo ! you got Blackjack"
-hasblackjack = true
-}
-else{
-    message = "you are out of game"
-    isalive = false
-}
-console.log(message)
+    if(sum < 22) {
+        message = "Do you want to draw a new card ?"
+    } else if(sum === 2){
+    message = "wohoo ! you got Blackjack"
+    hasblackjack = true
+    }
+    else{
+        message = "you are out of game"
+        isalive = false
+    }
+    instructionEl.textContent = message
+    sumEl.textContent = "sum:" + sum
+    cardEl.textContent = "cards:" + fristcard + " " + secondcard
 }
