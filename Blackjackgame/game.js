e6760df9626e399f7c754be5ea864ae703c6b869
satyam kale <satyamkale271@gmail.com>
2023-04-25 16:40:47 +0530

    made user interface for game

--- Blackjackgame/game.js
+++ Blackjackgame/game.js
@@ -3,15 +3,17 @@ let secondcard = 11
 let sum = fristcard + secondcard
 let hasblackjack = false    // this is boolean type value learned in this project //
 let isalive = true
-
+let message = ""
+function startgame() {
 if(sum < 22) {
-    console.log("Do you want to draw a new card")
+    message = "Do you want to draw a new card"
 } else if(sum === 2){
-console.log("wohoo ! you got Blackjack")
+message = "wohoo ! you got Blackjack"
 hasblackjack = true
 }
 else{
-    console.log("you are out of game")
+    message = "you are out of game"
     isalive = false
 }
-
+console.log(message)
+}
