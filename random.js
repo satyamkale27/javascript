9d2334f30fb7d8709ca91963f5ccc8a54ad2a2a7
satyam kale <satyamkale271@gmail.com>
2023-04-30 16:47:42 +0530

    learned random function in js

---
+++ random.js
@@ -0,0 +1,11 @@
+// this function generates random number between 0 to 1 but it does not includes 1
+// ex: 0.3265 0.9999  //
+// let randomnum = Math.random()
+// console.log(randomnum)
+
+// math.floor function in javascript //
+// let floors = Math.floor(6.23145)
+// console.log(floors)
+// it removes the decimal point of number //
+let randomnumber = Math.floor( Math.random() * 6) + 1
+console.log(randomnumber)
