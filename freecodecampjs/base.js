f5ee098b2ae23dcf82b050c858563510eba2c5cc
satyam kale <satyamkale271@gmail.com>
2023-06-12 17:11:20 +0530

    base4  added

--- freecodecampjs/base.js
+++ freecodecampjs/base.js
@@ -5,4 +5,10 @@ let carName;
 var city = "navi mumbai";
 var city;
 // here city will not loose its value //
+// string comparision in javascript //
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+console.log(result);
+// answer is true //
 
