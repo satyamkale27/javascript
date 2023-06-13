835efcac0e94f3e5daef9035cd656e171e1beec6
satyam kale <satyamkale271@gmail.com>
2023-06-13 22:53:44 +0530

    loop done

---
+++ freecodecampjs/whileloop.js
@@ -0,0 +1,34 @@
+//   while loop basic syntax //
+//      while (condition) {
+//     // code //
+// }
+let i = 0;
+while (i < 5){
+    console.log("hii");
+    i++;
+}
+
+// The “do…while” loop syntax  //
+// do {
+//     // loopbody //
+// } while (condition);
+
+
+let l = 0;
+do {
+    console.log(l);
+    l++;
+} while (l < 3)
+
+
+// The “for” loop syntax //
+// for (begin; condition; step) {
+     // ... loop body ...
+//   }
+ for (let p = 0; p < 4; p++){
+    console.log("aot");
+ }
+ // Here, the “counter” variable i is declared right in the loop.
+ // This is called an “inline” variable declaration.
+ // Such variables are visible only inside the loop.
+
