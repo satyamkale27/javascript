976b7767e4a1c3a58959979ab6432efb6ca4651d
satyam kale <satyamkale271@gmail.com>
2023-06-15 21:22:40 +0530

    logical1 added

---
+++ freecodecampjs/logical.js
@@ -0,0 +1,8 @@
+// it is logical operator in js //
+// if any one condition gets true it gets excuted //
+let hour = 9;
+if(hour < 10 || hour > 18) {
+console.log("yes");
+} else {
+console.log("no");
+}
