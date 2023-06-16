447214e57c42cdaceceb034b077ba4c09cfda157
satyam kale <satyamkale271@gmail.com>
2023-06-16 15:25:40 +0530

    notes added

--- freecodecampjs/function.js
+++ freecodecampjs/function.js
@@ -21,3 +21,13 @@ show(); // output 9 //
  }
  names(); // output namee //
  console.log(usernamee);  // output namee //
+
+ // parameter in a function //
+
+ function showwmess(form, text) {
+    console.log(form + ' : ' + text);
+ }
+ showwmess('ann', 'hello!'); // we have passed parameters to function // and also called //
+ // when value is passe into function it is called argument //
+ // If a function is called, but an argument is not provided, then the corresponding value becomes undefined.//
+
