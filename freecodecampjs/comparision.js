ed4d499d7d7fce7ec4740d091f92720f2f1689de
satyam kale <satyamkale271@gmail.com>
2023-06-18 21:35:38 +0530

    learned comparision

---
+++ freecodecampjs/comparision.js
@@ -0,0 +1,9 @@
+// here we will learn comparision in js //
+// we will cover the topic Comparison with null and undefined //
+// For a strict equality check === //
+console.log(null === undefined); // answer is false //
+console.log(null == undefined); // answer is true //
+// comparing null value with zero //
+console.log( null > 0); // false //
+console.log( null == 0); // false //
+console.log( null >= 0); // true //
