a0e0e5fbaaa6a0afadd5218bbbc654c013cb2378
satyam kale <satyamkale271@gmail.com>
2023-06-18 20:32:28 +0530

    basicfun.js learned(prompt,confirm)

---
+++ freecodecampjs/basicfun.js
@@ -0,0 +1,10 @@
+// we will learn basic inbuilt functions in javascript //
+// prompt function //
+let age = prompt('How old are you?', 100);
+
+alert(`You are ${age} years old!`); // it takes value fron user //
+
+// confirm function //
+let result = confirm("are you boss")
+alert(result); // confirm function works on true or false //
+// are you boss  if i said ok then it become true if i said cancle then it become false //
