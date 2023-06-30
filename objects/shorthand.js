18bdcf5d83bbfe7e1e3f131b8e66bd6030cd3327
satyam kale <satyamkale271@gmail.com>
2023-06-30 15:31:37 +0530

    shorthand in objeccts  learned

---
+++ objects/shorthand.js
@@ -0,0 +1,22 @@
+function mydata(name, age) {
+
+return {
+name: name,
+age: age
+
+};
+
+};
+
+let el = mydata("name", 19);
+console.log(el.name);
+console.log(el.age);
+
+// Instead of name:name we can just write name, like this
+// function makeUser(name, age) {
+//     return {
+//       name, // same as name: name
+//       age,  // same as age: age
+//       // ...
+//     };
+//   }
