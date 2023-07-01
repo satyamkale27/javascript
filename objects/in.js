2c272b86d5be3d3cd68bc34e364dc1d2ec315065
satyam kale <satyamkale271@gmail.com>
2023-07-01 14:57:50 +0530

    in property

---
+++ objects/in.js
@@ -0,0 +1,14 @@
+let user = {};
+console.log( user.name === undefined); // true means "no such property"
+
+// Reading a non-existing property just returns undefined. 
+// So we can easily test whether the property exists:
+
+// use can also use //
+
+let smt = {
+age: 19,
+weight: 52
+};
+console.log("age" in smt ); // true smt.age exists  // 
+console.log("height" in smt ); // false smt.height does not exists //
