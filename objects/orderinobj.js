3322d85f2d10a722eb782e9e2b8ce8331dd16138
satyam kale <satyamkale271@gmail.com>
2023-07-01 15:35:26 +0530

    for  used in object

---
+++ objects/orderinobj.js
@@ -0,0 +1,12 @@
+// let see are objects orderd //
+let codes = {
+    "49": "Germany",
+    "41": "Switzerland",
+    "44": "Great Britain",
+    91: "india",
+    // ..,
+    "1": "USA"
+  };
+  for( let num in codes ) {
+  console.log(num); // 1, 41, 44, 49 // it  go in the ascending sorted order //
+  };
