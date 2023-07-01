c0018aa1bfb4d9012fe1b6e51ff59b67dff7bfaf
satyam kale <satyamkale271@gmail.com>
2023-07-01 15:23:48 +0530

    key in data

--- objects/in.js
+++ objects/in.js
@@ -13,3 +13,17 @@ weight: 52
 console.log("age" in smt ); // true smt.age exists  // 
 console.log("height" in smt ); // false smt.height does not exists //
 // tip:- use in property insted of using undefined //
+
+// for loop in objects //
+let data = {
+    name: "John",
+    age: 30,
+    isAdmin: true
+  };
+  
+  for (let key in data) {
+    // keys
+    console.log( key );  // name, age, isAdmin
+    // values for the keys
+    console.log( data[key] ); // John, 30, true
+  }
