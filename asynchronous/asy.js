d305a5cf7d0b400b260fc50d31702fadc15accad
satyam kale <satyamkale271@gmail.com>
2023-08-07 23:10:37 +0530

    call back function || arrow function uded

--- asynchronous/asy.js
+++ asynchronous/asy.js
@@ -1,12 +1,24 @@
 
-function one(call_two) {
-console.log(" step one compleated call step 2");
-call_two();
+// function one(call_two) {
+// console.log(" step one compleated call step 2");
+// call_two();
+// };
+
+// function two() {
+//     console.log(" step 2 ");
+// };
+
+// one(two);
+
+// arrow function and call bacck //
+let order = (call_production) => {
+console.log(" order placed, please call the production");
+call_production();
+
 };
 
-function two() {
-    console.log(" step 2 ");
+let production = () => {
+console.log("order received, starting production");
 };
 
-one(two);
-
+order(production);
