5f830bd58f152f99771c096cd24ad99247f72a92
satyam kale <satyamkale271@gmail.com>
2023-08-13 17:55:06 +0530

    async

---
+++ asynchronous/async.js
@@ -0,0 +1,21 @@
+let stocks = {
+    Fruits : ["strawberry", "grapes", "banana", "apple"],
+    liquid : ["water", "ice"],
+    holder : ["cone", "cup", "stick"],
+    toppings : ["chocolate", "peanuts"],
+ };
+ let is_shop_open = true;
+
+ async function order (){
+    try{
+        await abc;
+    }
+    catch(error){
+        console.log("abc doesn't exis", error);
+    }
+
+    finally{
+        console.log("runs code anyways")
+    }
+ }
+ order();
