27dd298de113efbc5c67a7a63383eba15a176dfa
satyam kale <satyamkale271@gmail.com>
2023-09-29 22:58:39 +0530

    destructure object in js

---
+++ prereact/destructure.js
@@ -0,0 +1,11 @@
+let collection = {
+  tittle: "Harry porter",
+  author: "jk rolin",
+};
+
+// const tittlee = collection.tittle;
+// const authorss = collection.author;
+// destructuring //
+const { tittle, author } = collection;
+console.log(author, tittle);
+// jk rolin Harry porter //
