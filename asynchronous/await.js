3487a0b108bb2b8a50471db2834fca92a7c06cec
satyam kale <satyamkale271@gmail.com>
2024-03-20 23:41:47 +0530

    typo fixed

--- asynchronous/await.js
+++ asynchronous/await.js
@@ -1,37 +1,31 @@
 let stocks = {
-    Fruits : ["strawberry", "grapes", "banana", "apple"],
-    liquid : ["water", "ice"],
-    holder : ["cone", "cup", "stick"],
-    toppings : ["chocolate", "peanuts"],
- };
- let is_shop_open = true;
+  Fruits: ["strawberry", "grapes", "banana", "apple"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "peanuts"],
+};
+let is_shop_open = true;
 
- let topping_choice = () =>{
-    return new promice( (resolve, reject)=>{
-   setTimeout(()=>{
-    resolve(
-        console.log("which toppin do you want")
-    );
-    
-   },3000);
+let topping_choice = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(console.log("which toppin do you want"));
+    }, 3000);
+  });
+};
 
-    } );
- };
+async function kitchen() {
+  console.log(" A ");
+  console.log(" B ");
+  console.log(" C ");
 
- async function kitchen() {
-    console.log(" A ");
-    console.log(" B ");
-    console.log(" C ");
- 
+  await topping_choice();
 
- await topping_choice();
+  console.log(" D ");
+  console.log(" E ");
+}
+kitchen();
 
- console.log(" D ");
- console.log(" E ");
-
- }
- kitchen();
-
- console.log("doing the dishes");
- console.log("cleaning the tables");
- console.log("taking other orders");
+console.log("doing the dishes");
+console.log("cleaning the tables");
+console.log("taking other orders");
