12be5a5f349259fbf50690fc92ebc38e89facd71
satyam kale <satyamkale271@gmail.com>
2024-03-23 18:35:26 +0530

    added gitignore file and learnes .then and fetch

---
+++ asynchronous/experiments.js
@@ -0,0 +1,12 @@
+import("node-fetch").then((module) => {
+  const fetch = module.default;
+  const fetchPromise = fetch(
+    "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+  );
+
+  console.log(fetchPromise);
+
+  // fetchPromise.then((response) => {
+  //   console.log(`Received response: ${response.status}`);
+  // });
+});
